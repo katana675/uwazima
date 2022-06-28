d205117bdadd4e250b56033a0cbc37048df68be9
katana <xzabreak2769cabjkin@gmail.com>
2022-06-29 07:18:33 +0900

    ss

---
+++ Desktop/Eclipse/private/src/main/java/servlet/Thank.java
@@ -0,0 +1,78 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Thank
+ */
+@WebServlet("/Thank")
+public class Thank extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Thank() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String view = "/WEB-INF/view/thank.jsp";
+	    RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+	    dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("name");
+	    String mail = request.getParameter("mail");
+	    String phone = request.getParameter("phone");
+	    String msg="";
+	    try {
+		      Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		      msg = "ドライバのロードに成功しました";
+		    }catch (ClassNotFoundException e){
+		    	System.out.println(e.getMessage());
+		      msg = "ドライバのロードに失敗しました1";
+		    }catch (Exception e){
+		      msg = "ドライバのロードに失敗しました2";
+		    }
+		System.out.println(msg);
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdb","root","");
+			String sql = "insert into item(name,mailaddress,tel)values(?,?,?)";	
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setString(1,name);
+			st.setString(2,mail);
+			st.setString(3, phone);
+			int result = st.executeUpdate();
+			st.close();
+            con.close();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		doGet(request, response);
+	}
+
+}
---
+++ Desktop/Eclipse/private/src/main/webapp/WEB-INF/view/input.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form method="post" action="./check">
+		<h3>名前 <input type="text" name="name"></h3><br>
+		<h3>メールアドレス <input type="text" name="mail"></h3><br>
+		<h3>電話番号<input type="text" name="phone"></h3><br>
+		<button type="submit">送信</button>
+	</form>
+</body>
+</html>
